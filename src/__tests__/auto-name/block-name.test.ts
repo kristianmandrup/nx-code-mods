da98f984ffa27057b5c613e68d0d0ac4b4408080
kristianmandrup <kmandrup@gmail.com>
2022-04-26 13:30:42 +0100

    more block name tests

--- src/__tests__/auto-name/block-name.test.ts
+++ src/__tests__/auto-name/block-name.test.ts
@@ -8,14 +8,56 @@ import { Block } from 'typescript';
 const context = describe;
 
 describe('block name', () => {
-  context('file with no named object', () => {
+  context('user find in last statement', () => {
     it('determines sensible name', () => {
-      const filePath = path.join(__dirname, 'files', 'user-find-block.txt');
+      const filePath = path.join(
+        __dirname,
+        'files',
+        'user-find-last-statement.txt',
+      );
       const content = readFileIfExisting(filePath);
       const srcNode = tsquery.ast(content);
       const block = findFunctionBlock(srcNode, 'xyz') as Block;
       const name = blockName(block);
       expect(name).toEqual('findUserWhereType');
+    });
+  });
+
+  context('return user in last statement', () => {
+    it('determines sensible name', () => {
+      const filePath = path.join(
+        __dirname,
+        'files',
+        'user-find-return-user.txt',
+      );
+      const content = readFileIfExisting(filePath);
+      const srcNode = tsquery.ast(content);
+      const block = findFunctionBlock(srcNode, 'xyz') as Block;
+      const name = blockName(block);
+      expect(name).toEqual('findUserWhere');
+    });
+  });
+
+  context('return user or default', () => {
+    it('determines sensible name', () => {
+      const filePath = path.join(__dirname, 'files', 'user-find-default.txt');
+      const content = readFileIfExisting(filePath);
+      const srcNode = tsquery.ast(content);
+      const block = findFunctionBlock(srcNode, 'xyz') as Block;
+      const name = blockName(block);
+      expect(name).toEqual('findUserWhere');
+    });
+  });
+
+  context('users sort', () => {
+    it('determines sensible name', () => {
+      const filePath = path.join(__dirname, 'files', 'users-sort.txt');
+      const content = readFileIfExisting(filePath);
+      const srcNode = tsquery.ast(content);
+      const block = findFunctionBlock(srcNode, 'xyz') as Block;
+      const name = blockName(block);
+      console.log({ name });
+      expect(name).toEqual('sortedUsersBy');
     });
   });
 });
